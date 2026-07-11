55fe8d606a168c26673b0abb2ba1d3520ce11b65
Roshan Giyanani <roshangiyanani@gmail.com>
2026-07-11 19:42:59 -0400

    fix __main__.py and improve its ergonomics

--- cs336_basics/bpe_tokenizer/__main__.py
+++ cs336_basics/bpe_tokenizer/__main__.py
@@ -3,7 +3,7 @@ import logging
 from pathlib import Path
 
 from tqdm import tqdm
-from tqdm.contrib.logging import logging_redirect_tqdm
+from tqdm.contrib.logging import logging_redirect_tqdm, tqdm_logging_redirect
 
 from cs336_basics.bpe_tokenizer.naive_tokenizer import NaiveTokenizer
 from cs336_basics.bpe_tokenizer.pretokenizer import GPT_RE, SimplePretokenizer
@@ -11,15 +11,16 @@ from cs336_basics.bpe_tokenizer.segmenter import BufferingSegmenter, InMemorySeg
 
 logger = logging.getLogger(__name__)
 
-SPECIAL_TOKENS = [b"<|endoftext|>"]
+SPECIAL_TOKENS = ["<|endoftext|>"]
 
 
 def main():
-    logging.basicConfig(level=logging.INFO, format="%(name)s - %(message)s")
+    logging.basicConfig(format="%(name)s - %(message)s")
+    logger.level = logging.INFO
 
     parser = argparse.ArgumentParser(description="Train a BPE tokenizer on a text file.")
     parser.add_argument("filepath", help="Path to the input text file")
-    parser.add_argument("--merges", type=int, default=6, help="Number of BPE merges to perform")
+    parser.add_argument("--vocab-size", type=int, default=1000, help="Desired vocab size.")
     args = parser.parse_args()
 
     # segmenter = InMemorySegmenter(SPECIAL_TOKENS)
@@ -33,12 +34,16 @@ def main():
     pretokenized_counts = pretokenizer.finalize()
 
     tokenizer = NaiveTokenizer(pretokenized_counts, SPECIAL_TOKENS)
-    logger.info("Training: %d merges", args.merges)
-    tokenizer.merge_n(args.merges)
+    logger.info("Training: %d vocab size", args.vocab_size)
+    vocab_size = len(tokenizer.vocab)
+    with tqdm_logging_redirect():
+        for i in tqdm(range(args.vocab_size - vocab_size)):
+            tokenizer.merge_once()
+
     vocab, merges = tokenizer.as_output()
 
     logger.info("Training complete. Vocab size: %d, Merges: %d", len(vocab), len(merges))
-    logger.info("Merges: %s", merges)
+    logger.debug("Merges: %s", merges)
 
 
 if __name__ == "__main__":
